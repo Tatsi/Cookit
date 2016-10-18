4ecd5682c0271cd8d85e0f1e1c9be94bafb56fa1
Jenni Pajukoski <jenni@giosg.com>
2016-10-18 16:30:54 +0300

    More stuff to recipe page

--- main/views.py
+++ main/views.py
@@ -16,7 +16,38 @@ def feed(request):
 	return render(request, 'feed.html', context)
 
 def recipepage(request):
-	context = {}
+	name = 'Chicken soup'
+	favourite = True
+	username = 'Mary_1982'
+	description = 'Pure soul food, this warming, easy chicken soup makes the most of leftover roast chicken.'
+	time = '1 h'
+	servings = 6
+	ingredients = [
+		{'amount': '55 g', 'name': 'butter'},
+		{'amount': '2', 'name': 'onions'},
+		{'amount': '2 sticks', 'name': 'celery'},
+		{'amount': '2', 'name': 'carrots'},
+		{'amount': '25 g', 'name': 'flour'},
+		{'amount': '450 g', 'name': 'chicken'},
+		{'amount': '1 tbsp', 'name': 'parsley'},
+		{'amount': '1 tsp', 'name': 'black pepper'},
+	]
+	instructions = [
+		'Melt the butter in a large saucepan over a medium heat and gently fry the onions, celery and carrots until they start to soften.',
+		'Stir in the flour and cook for 2 minutes. Add the chicken stock and bring the mixture to the boil, stirring as you do so. Season with salt and pepper, then reduce the heat until the mixture is simmering and simmer for 10 minutes, until the vegetables are tender.',
+		'Add the cooked chicken and cook until heated through. Adjust the seasoning, stir in the parsley and serve.',
+		'Eat'
+	]
+	context = {
+		'name': name,
+		'favourite': favourite,
+		'username': username,
+		'description': description,
+		'time': time,
+		'servings': servings,
+		'ingredients': ingredients,
+		'instructions': instructions,
+	}
 	return render(request, 'recipepage.html', context)
 
 def register(request):
