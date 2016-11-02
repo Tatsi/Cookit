c33fa006ee11696ed8253cda473aa4084a45e239
Joni Anttalainen <joni.anttalainen@gmail.com>
2016-11-02 18:32:37 +0200

    Base for Recipe model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    title = models.CharField(max_length=300)
+    image_url = models.CharField(max_length=600)
+    steps = models.CharField(max_length=10000, blank=True)
+    # TODO creator
+    # TODO ingredients
+    rating_count = models.BigIntegerField()
+    average_rating = models.DecimalField(max_digits=10, decimal_places=9)
