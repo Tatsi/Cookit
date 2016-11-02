6ef5f5e9981d25d8306d16aecbc08a0c28b12b5a
Joni Anttalainen <joni.anttalainen@gmail.com>
2016-11-02 19:39:28 +0200

    Models: Added relations between models

--- main/models.py
+++ main/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 
 class UserAccount(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # TODO favourite recipes
-    # TODO favourite users
-    # TODO history_recipes
+    favourites_recipes = models.ManyToManyField('Recipe', related_name="%(app_label)s_%(class)s_favourites")
+    favourite_users = models.ManyToManyField('self')
+    history_recipes = models.ManyToManyField('Recipe', related_name="%(app_label)s_%(class)s_history")
 
 class Recipe(models.Model):
     # id = models.BigIntegerField(primary_key=True) # Not needed
     title = models.CharField(max_length=300)
     image_url = models.CharField(max_length=600)
     steps = models.CharField(max_length=10000, blank=True)
-    # TODO creator
-    # TODO ingredients
+    creator = models.ForeignKey(UserAccount)
+    ingredients = models.ManyToManyField('Ingredient', through='RecipeIngredient')
     rating_count = models.BigIntegerField()
     average_rating = models.DecimalField(max_digits=10, decimal_places=9)
 
@@ -30,4 +30,6 @@ class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe)
     ingredient = models.ForeignKey(Ingredient)
     amount = models.CharField(max_length=100)
+
+
 
