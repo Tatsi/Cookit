cf8381ff9b31109a6e49b08be1d2899c3d25bc96
Joni Anttalainen <joni.anttalainen@gmail.com>
2016-11-11 14:22:32 +0200

    Updated fixture loader script

--- load_fixture_data.sh
+++ load_fixture_data.sh
@@ -1,4 +1,4 @@
 # 10 user accounts are created, user1-user10, their passwords are "password".
 
 python manage.py loaddata main/fixtures/ingredients_2016-11-06_22_48_35.json
-python manage.py loaddata main/fixtures/others_2016-11-10_20_02_11.json
+python manage.py loaddata main/fixtures/others_2016-11-11_14_19_46.json
