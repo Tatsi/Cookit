6bbd895d3592fec0a2b5bc3e16f5ad3f2a7dbcfd
Severi Salminen <severi.salminen@iki.fi>
2016-12-29 19:33:57 +0200

    Added media to url even with debug=false

--- cookit/urls.py
+++ cookit/urls.py
@@ -43,5 +43,4 @@ urlpatterns = [
     url(r'^recipes/new/$', main.views.new_recipe, name='new_recipe'),
 ]
 
-if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
